4ea7339d1188347d5d6cea6613563a0fc9496071
neatsopheng <nsopheng13@gmail.com>
2024-01-28 14:38:33 +0700

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
-import GameGrid from './components/MainPage/GameGrid'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import GameGrid from "./components/MainPage/GameGrid";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
-    <>
-      <GameGrid/>
-    </>
-  )
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+       {/* show how our page is gonna be layout */}
+      <GridItem area="nav">
+        <Navbar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,13 @@
+import { HStack, Image } from "@chakra-ui/react"
+import logo from '../../assets/react.svg';
+import ColorModeSwitch from "../colorModeSwitch";
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <HStack justifyContent={'space-between'} padding={'10px'}>
+      <Image src={logo} boxSize={'60px'} backgroundColor={'transparent'}/>
+      <ColorModeSwitch />
+    </HStack>
   )
 }
 
