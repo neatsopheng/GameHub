37a442a407e11f8e7080af97799f4b01183241c7
neatsopheng <nsopheng13@gmail.com>
2024-02-02 11:46:46 +0700

    Showing spinner

--- src/components/Aside/GenreList.tsx
+++ src/components/Aside/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   // const {data} = useData<Genre>('/genres'); //bad practice: neeed to avoid component about knowing the endpoint
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner />
+
+  if (error) return null;
+
   return (
     <List>
       {data.map((genre) => (
