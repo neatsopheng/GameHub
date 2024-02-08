37e3aeb2243f89094d591745ab31c2d7b42635f4
neatsopheng <nsopheng13@gmail.com>
2024-02-08 11:41:54 +0700

    Refcator GameGrid

--- src/components/MainPage/GameGrid.tsx
+++ src/components/MainPage/GameGrid.tsx
@@ -14,10 +14,9 @@ function GameGrid({gameQuery}: Props) {
   const { data, error, isLoading } = useGame(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  if (error) return <Text>{error}</Text>
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={3}
@@ -35,7 +34,6 @@ function GameGrid({gameQuery}: Props) {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 }
 
